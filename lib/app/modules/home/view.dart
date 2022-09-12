e590f4cdb3ccfcabdde90b4fabddcc66f21f8225
Yoshi <dleonavichus@mail.ru>
2022-09-12 19:56:22 +0300

    date_picker

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var selectedDay = DateTime(2022, 9, 1);
+  var selectedDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
     var tag = Localizations.maybeLocaleOf(context)?.toLanguageTag();
